eac9bb57a42102067b584acff13a78dfa4ecea95
UnspeakableSnow <yukkip45@gmail.com>
2024-05-27 13:50:41 +0900

    0.0.4

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # image-collecter
 
 An image collection tool that collects images by URL name and also collects them in English and Chinese.
+
+```python
+import image_collect
+
+image_collect.run("jump cat", en_search=False)
+```
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fp:
 
 setup(
     name='image_collect',
-    version='0.0.3',
+    version='0.0.4',
     py_modules=['image_collect'],
     install_requires=["BeautifulSoup4","deep_translator"],
     url='https://github.com/ohS0eek4/image-collect',
