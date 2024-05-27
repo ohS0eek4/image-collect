3d2826283c03c7e81cb867801290f8db54a32aff
UnspeakableSnow <yukkip45@gmail.com>
2024-05-27 13:45:38 +0900

    応急

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
+with open('README.md', 'r') as fp:
+    readme = fp.read()
 
 setup(
     name='image_collect',
-    version='0.0.1',
+    version='0.0.3',
     py_modules=['image_collect'],
     install_requires=["BeautifulSoup4","deep_translator"],
+    url='https://github.com/ohS0eek4/image-collect',
+    long_description=readme,
+    long_description_content_type='text/markdown'
 )
